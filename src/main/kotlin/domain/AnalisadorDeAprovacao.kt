72e13e4202ec47b391d5e848b86e702f6090f6b4
Mohamad Rkein <mohamad_rkein@usp.br>
2022-05-15 21:58:51 -0300

    Define methods defineCriterio and fechaBoletim for AnalisadorDeAprovacao class

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,22 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    private lateinit var criterio: CriterioDeAprovacao
+
+    fun defineCriterio(criterio: CriterioDeAprovacao) {
+        this.criterio = criterio
+    }
+
+    fun fechaBoletim(boletim: Boletim): BoletimFechado {
+        return BoletimFechado(
+            mediaEPs = boletim.mediaEPs, 
+            mediaMiniEPs = boletim.mediaMiniEPs, 
+            mediaFinal = this.criterio.mediaFinal(boletim), 
+            foiAprovado = this.criterio.estaAprovado(boletim)
+        )
+    }
 
 }
